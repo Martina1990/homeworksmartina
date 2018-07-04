7f8136e595ac104577fc4de976043ecc583f494f
Martina Wabin <martina.wabin@outlook.at>
2018-07-04 21:26:39 +0200

    added secret number homework

---
+++ secretnumber.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+secret = 9
+guess = int(raw_input("Guess the secret number:"))
+if secret == guess:
+    print "You won!"
+else:
+    print "You lost!"
